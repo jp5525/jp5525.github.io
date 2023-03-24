12c968b38355ff29f898c98c01e7b6f6d0edcbf4
Jared Piro <jp5525@yahoo.com>
2023-03-23 23:59:17 -0400

    Updates

---
+++ _next/static/76-AvMJ1tFkNEd-bnVgnB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/0-3f4017f784d0e784.js","static/css/a978709d6f25d309.css","static/chunks/pages/index-3ec7702961ac95f4.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
